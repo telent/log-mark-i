d998a55a2b305df028c4883e3560cccaecd5c6b3
Daniel Barlow <dan@telent.net>
2020-06-12 20:54:14 +0100

    convert to using json config

--- claphands/__init__.py
+++ claphands/__init__.py
@@ -21,13 +21,12 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        WITHINGS_AUTH=withings_auth(yaml.load(open(os.path.join(app.instance_path, 'withings_oauth.json')))),
         CREDENTIAL_STORE_PATH=os.path.join(app.instance_path, 'credentials.pickle'),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_json('config.json', silent=True)
     else:
         # load the test config if passed in
         app.config.from_mapping(test_config)
--- claphands/withings.py
+++ claphands/withings.py
@@ -2,12 +2,26 @@ from urllib import parse
 import secrets
 
 from flask import current_app, g, Blueprint, request, make_response, redirect
+from withings_api import WithingsAuth, AuthScope
 
 from claphands.credentials import get_credential_store
 
+def withings_auth(config):
+    return WithingsAuth(
+        client_id=config['WITHINGS_CLIENT_ID'],
+        consumer_secret=config['WITHINGS_CONSUMER_SECRET'],
+        callback_uri=config['WITHINGS_CALLBACK'],
+        scope=(
+            AuthScope.USER_ACTIVITY,
+            AuthScope.USER_METRICS,
+            AuthScope.USER_INFO,
+            AuthScope.USER_SLEEP_EVENTS,
+        )
+    )
+
 def get_withings_auth():
     if 'withings_auth' not in g:
-        g.withings_auth = current_app.config['WITHINGS_AUTH']
+        g.withings_auth = withings_auth(current_app.config)
     return g.withings_auth
 
 def new_token():

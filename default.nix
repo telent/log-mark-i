ab1b30da5849d98f2e1237675f3c144ec1889839
Daniel Barlow <dan@telent.net>
2020-07-01 11:21:59 +0100

    export PYTHONPATH in server startup script

--- default.nix
+++ default.nix
@@ -1,17 +1,21 @@
 with import <nixpkgs> {};
 let
   python = import ./requirements.nix { inherit pkgs; };
-  logmarki = python.mkDerivation {
+  logmarki_pkg = python.mkDerivation {
     pname = "log-mark-i";
     version = "0.1.0";
     src = ./.;
     buildInputs = builtins.attrValues python.packages;
     propagatedBuildInputs = builtins.attrValues python.packages;
-  };
-  # I elected not to put the config file in /nix/store because it
-  # contains oauth2 secrets so maybe doesn't want to be world-readable.
-  # You can write a derivation that does otherwise if you like.
-  in pkgs.writeScriptBin "server" ''
-PYTHONPATH=${logmarki}/lib/python3.7/site-packages/:$PYTHONPATH
-INSTANCE_PATH=/etc/log-mark-i ${python.interpreter}/bin/gunicorn 'logmarki:create_app()'
+    # I use this with a systemd unit that runs
+    #
+    #   INSTANCE_PATH=/etc/log-mark-i
+    #   ${python.interpreter}/bin/gunicorn 'logmarki:create_app()'
+    #
+    # I elected not to put the config file in /nix/store because it
+    # contains oauth2 secrets so maybe doesn't want to be world-readable.
+  }; in
+pkgs.writers.writeBashBin "log-mark-i-server" ''
+  export PYTHONPATH=${logmarki_pkg}/lib/python3.7/site-packages/:$PYTHONPATH
+  ${python.interpreter}/bin/gunicorn 'logmarki:create_app()'
 ''

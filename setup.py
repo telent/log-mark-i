1979c06518a976e183cf622ea70d9b693f8e9ebe
Daniel Barlow <dan@telent.net>
2020-07-01 17:36:06 +0100

    remove pypkgs.json, unneeded now we use pypi2nix

--- setup.py
+++ setup.py
@@ -1,16 +1,19 @@
 from setuptools import find_packages, setup
 import json
 
-def requirements():
-    # we keep requirements in a separate file to make it easier for nix
-    with open("pypkgs.json","r") as fh:
-        return json.load(fh)
-
 setup(
     name='logmarki',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=requirements()
+    install_requires=[
+        "flask",
+        "gunicorn",
+        "pip",
+        "pylint",
+        "pyyaml",
+        "setuptools",
+        "withings_api"
+    ]
 )
